1bc6d3c944deb09265f6d079404a3f4dba8c9ce3
Agustin Salinas <chaucharian@gmail.com>
2020-04-09 11:56:35 -0300

    color transition added

--- speechReconigtion.js
+++ speechReconigtion.js
@@ -1,56 +1,79 @@
- let recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
+const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-  recognition.onstart = () => {
-    paintScreen('black');
-  }
-  // recognition.onend = () => console.log(" END! ");
+const colors = ['azul', 'rojo', 'verde', 'amarillo', 'negro', 'blanco', 'naranja','violeta'];//, 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
-  recognition.onresult = (event) => {
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      const statement = event.results[i][0].transcript;
-      paintScreen(isAnyColor(statement));
-    }
-  }
-  const isAnyColor = incommingStatement => {
-    let currentColor = '';
-    const normalizedStatement = incommingStatement.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    const statement = normalizedStatement.split(" ")[normalizedStatement.split(" ").length-1];
-    if (statement.includes('rojo')) {
-      currentColor = 'red';
-    } else if (statement.includes('azul')) {
-      currentColor = 'blue'
-    } else if (statement.includes('amarillo')) {
-      currentColor = "#ffdf00"
-    }  else if (statement.includes('verde')) {
-      currentColor = "green"
-    } else if (statement.includes('negro')) {
-      currentColor = "black"
-    } else if (statement.includes('marron')) {
-      currentColor = "brown"
-    } else if (statement.includes('naranja')) {
-      currentColor = "#ff5e00"
-    } else if (statement.includes('violeta')) {
-      currentColor = "violet"
-    } else if (statement.includes('blanco')) {
-      currentColor = "white"
-    }
-    document.getElementById('speech').innerText = currentColor;
-    return currentColor;
-  }
+const recognition = new SpeechRecognition();
+const speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'es-Es';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
-  const paintScreen = color => {
-    const body = document.getElementsByTagName('body')[0];
-    if(color === 'white') {
-      document.getElementsByTagName('h1')[0].style.color = 'black';
-    } else if(color === 'black') {
-      document.getElementsByTagName('h1')[0].style.color = 'white';
-    }
-    body.style.backgroundColor = color;
+recognition.onresult = (event) => {
+  const color = event.results[0][0].transcript.split(" ")[event.results[0][0].transcript.split(" ").length - 1].toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  paintScreen(isAnyColor(color));
+}
+const isAnyColor = color => {
+  let outputColor = '';
+  switch (color) {
+    case 'rojo':
+      outputColor = 'red';
+      break;
+    case 'azul':
+      outputColor = 'blue';
+      break;
+    case 'amarillo':
+      outputColor = '#ffdf00';
+      break;
+    case 'naranja':
+      outputColor = '#ff5e00';
+      break;
+    case 'verde':
+      outputColor = 'green';
+      break;
+    case 'negro':
+      outputColor = 'black';
+      break;
+    case 'marron':
+      outputColor = 'brown';
+      break;
+    case 'violeta':
+      outputColor = '#a30dd4';
+      break;
+    case 'blanco':
+      outputColor = 'white';
+      break;
   }
+  document.getElementById('speech').innerText = color;
+  return outputColor;
+}
 
-  document.getElementsByTagName('button')[0].addEventListener('mousedown', () => recognition.start(), false);
-  document.getElementsByTagName('button')[0].addEventListener('mouseup', () => recognition.stop(), false);
-  document.getElementsByTagName('button')[0].addEventListener('touchstart', () => recognition.start(), false);
-  document.getElementsByTagName('button')[0].addEventListener('touchend', () => recognition.stop(), false);
+const paintScreen = color => {
+  const body = document.getElementsByTagName('body')[0];
+  if (color === 'white') {
+    document.getElementsByTagName('h1')[0].style.color = 'black';
+  } else if (color === 'black') {
+    document.getElementsByTagName('h1')[0].style.color = 'white';
+  }
+  body.style.backgroundColor = color;
+}
+
+const button = document.getElementsByTagName('button')[0];
+const startRecording = () => {
+  button.style.border = '2px solid red';
+  recognition.start();
+};
+const stopRecording = () => {
+  button.style.border = '2px solid white';
+  recognition.stop();
+};
+
+button.addEventListener('mousedown', () => startRecording(), false);
+button.addEventListener('mouseup', () => stopRecording(), false);
+button.addEventListener('touchstart', () => startRecording(), false);
+button.addEventListener('touchend', () => stopRecording(), false);
